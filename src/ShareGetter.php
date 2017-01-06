1daef64fc5c660034b13bf04f21b75d8f8ad20e9
Chris Herbert <herbert@bermanco.com>
2017-01-05 23:37:49 -0500

    added speed improvements for repeat requests,  methods for getting specific share and comment counts

--- src/ShareGetter.php
+++ src/ShareGetter.php
@@ -9,6 +9,7 @@ class ShareGetter {
 	protected $app_id;
 	protected $app_secret;
 	protected $access_token;
+	protected $cached_requests = array();
 
 	/////////
 	// Set //
@@ -41,24 +42,58 @@ class ShareGetter {
 			return $this->app_id . '|' . $this->app_secret;
 		}
 
-		return 'You must provide an access token OR both the app ID and app secret.';
+		throw new \Exception('You must provide an access token OR both the app ID and app secret.');
 
 	}
 
-	public function get_shares($url){
+	public function get_data($url){
+
+		if (isset($this->cached_requests[$url])){
+			return $this->cached_requests[$url];
+		}
 
 		$curl = new Curl;
 		$curl->setOpt(CURLOPT_FOLLOWLOCATION, true);
 
-		$url = $this->create_facebook_request_url($url);
+		$request_url = $this->create_facebook_request_url($url);
 
-		$curl->get($url);
+		$curl->get($request_url);
 
 		if ($curl->error){
 			return 'Error: ' . $curl->errorCode . ': ' . $curl->errorMessage;
 		}
 
-		return $curl->response;
+		$data = $curl->response;
+
+		$this->cached_requests[$url] = $data;
+
+		return $this->cached_requests[$url];
+
+	}
+
+	public function get_share_count($url){
+
+		$data = $this->get_data($url);
+
+		if (isset($data->share->share_count)){
+			return $data->share->share_count;
+		}
+		else {
+			return 0;
+		}
+
+	}
+
+	public function get_comment_count($url){
+
+		$data = $this->get_data($url);
+
+		if (isset($data->share->comment_count)){
+			return $data->share->comment_count;
+		}
+		else {
+			return 0;
+		}
 
 	}
 
@@ -71,11 +106,15 @@ class ShareGetter {
 		$base_url = 'https://graph.facebook.com/v2.7';
 
 		$params = array(
-			'id' => urlencode($url),
+			'id' => $url,
 			'access_token' => $this->get_access_token()
 		);
 
-		return $base_url . '?' . http_build_query($params);
+		$url = $base_url . '?' . http_build_query($params);
+
+		error_log($url);
+
+		return $url;
 
 	}
 
